f549d0e25c16fac0652be6c090b5562996186176
MACBOOK <macbook@qml78-h01-176-168-88-196.dsl.sta.abo.bbox.fr>
2023-04-21 10:36:07 -0700

    ajuste

--- acao.php
+++ acao.php
@@ -5,14 +5,25 @@ if(!$_SESSION["usuario"]){
     header("location:index.php");
 }
 
+$stmt = $pdo->prepare("SELECT * FROM usuario_produto WHERE usuario_id=:usuario and produto_id=:produto " );
+    $stmt->execute(['usuario' => $_SESSION['usuario']["id"], "produto" => $_POST["produto"]]); 
+    $dados = [];
+    $data = $stmt->fetchAll();
+    foreach ($data as $row) {
+         $dados[] = $row;
+    }
 
-try{
-    $sql = "INSERT INTO usuario_produto (usuario_id, produto_id, gostei,data_cadastro) VALUES (?,?,?,?)";
-    $stmt= $pdo->prepare($sql);
-    $stmt->execute([$_SESSION["usuario"]["id"], $_POST["produto"], $_POST["valor"],date("Y-m-d H:i:s")]);
-    
-}catch(Exception $e){
-    print_r($e->getMessage());
-}
+    if(!$dados){
+        try{
+            $sql = "INSERT INTO usuario_produto (usuario_id, produto_id, gostei,data_cadastro) VALUES (?,?,?,?)";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$_SESSION["usuario"]["id"], $_POST["produto"], $_POST["valor"],date("Y-m-d H:i:s")]);
+            
+        }catch(Exception $e){
+            print_r($e->getMessage());
+        }
+    }
+
+
 
 ?>
